b70bcc6c06cae87f6408fbb473f95c7a3e312b7b
김동현 <kimgguggury1002@gmail.com>
2025-02-02 15:50:32 +0900

    3장 학습 2/2

---
+++ 3장/예제/3-6.py
@@ -0,0 +1,20 @@
+import cv2 as cv
+import sys
+import matplotlib.pyplot as plt
+
+img = cv.imread('image/gguggury.jpg')
+
+if img is None :
+    sys.exit("파일이 존재하지 않음")
+
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+plt.imshow(gray, cmap='gray'), plt.xticks([]), plt.yticks([]), plt.show()
+
+h = cv.calcHist([gray], [0], None, [256], [0, 256])
+plt.plot(h, color='r',linewidth=1), plt.show()
+
+equal = cv.equalizeHist(gray)
+plt.imshow(equal, cmap='gray'), plt.xticks([]), plt.yticks([]), plt.show()
+
+h = cv.calcHist([equal], [0], None, [256], [0, 256])
+plt.plot(h, color='r',linewidth=1), plt.show()
